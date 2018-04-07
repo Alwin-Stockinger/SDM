daf0189da6778c9c65e328ab40e2fdbb020bc8d3
Bareis Klaus <uni@B4BD>
2018-04-07 20:15:47 +0200

    count and map example

--- SDM2/src/main/RunSpark.java
+++ SDM2/src/main/RunSpark.java
@@ -3,8 +3,19 @@ package main;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.calcite.linq4j.Linq4j;
+import org.apache.log4j.Logger;
+import org.apache.log4j.varia.NullAppender;
 import org.apache.spark.SparkConf;
+//import java.awt.List;
+import java.util.Arrays;
+import java.util.List;
 
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 
 class RunSpark{
 
@@ -15,14 +26,39 @@ class RunSpark{
 
 		// local environment (laptop/PC)
 		SparkConf conf = new SparkConf().setAppName("GRUPPEXX").setMaster("local[*]");
-
+		
+		
 		JavaSparkContext sc = new JavaSparkContext(conf);
+		sc.setLogLevel("ERROR");
+
+				
 		String AWS_ACCESS_KEY_ID = "";
 		String AWS_SECRET_ACCESS_KEY = "";
 
-		System.out.println(">>>>>>>>>>>>>>>>>> Hello from Spark! <<<<<<<<<<<<<<<<<<<<<<<<<");
+		{	// count
+			JavaRDD<String> textLoadRdd=sc.textFile("./src/main/RunSpark.java");
+			System.out.println("Lines:"+textLoadRdd.count());
+		}
 
-//  ...
+		{	// map
+			List data=(List) Arrays.asList(1,2,3,4,5);			System.out.println("data="+data);
+			JavaRDD<Integer> distData=sc.parallelize((java.util.List<Integer>) data);
+			JavaRDD<Integer> manipulated=distData.map(
+					new Function<Integer,Integer>()  {
+						public Integer call(Integer myInt) {
+							myInt=myInt*2;
+							//System.out.println(myInt);
+							return myInt;
+						}
+					}
+			);
+			//JavaRDD<Integer> manipulated =distData.map(n ->new Integer(n) *2);
+			List<Integer> tranformed = manipulated.collect();
+			System.out.print("tranformed:"); for (Integer d : tranformed) System.out.print(d +" ");	System.out.println("");
+		}
+		
+		
+
 
 		// example accessing S3:
 
