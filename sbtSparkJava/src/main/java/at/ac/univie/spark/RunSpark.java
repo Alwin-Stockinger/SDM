1104ee6569949a529bdfed66d8fbe1517870d38a
mrberkay <berqqay@hotmail.com>
2018-04-24 15:08:53 +0200

    listed clustering labels + Remove all non numeric features, to prepare rdd for Kmeans

--- sbtSparkJava/src/main/java/at/ac/univie/spark/RunSpark.java
+++ sbtSparkJava/src/main/java/at/ac/univie/spark/RunSpark.java
@@ -1,32 +1,97 @@
 package at.ac.univie.spark;
 
 import org.apache.spark.api.java.JavaSparkContext;
+
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.rdd.RDD;
+
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.JavaPairRDD;
+
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 import org.apache.spark.SparkConf;
 
 
-class RunSpark{
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
 
+
+class RunSpark{
+    
+	
+	// Remove all non numeric features
+	private static JavaRDD<Vector>  parse(JavaRDD<String> rdd) 
+    {
+        return rdd.map(
+                (Function<String, Vector>) s -> { String[] splitted = s.split(",");
+				
+                    double[] values = new double[splitted.length-4];
+					
+                    int j = 0;
+					
+                    for (int i = 0; i < splitted.length; i++) 
+					{
+                        if(i!=1 && i!=2 && i!=3 &&i!=41)
+						{
+                            values[j] = Double.parseDouble(splitted[i]);
+                            j++;
+                        }
+
+                    }
+                    return Vectors.dense(values);
+                }
+        );
+    }
+	
+	//List the clustering labels (last column) and their distinct counts
+	private static void countLabels(JavaRDD<String> rdd){
+        try{
+            PrintWriter writer = new PrintWriter("labels"+".txt", "UTF-8");
+            
+            JavaRDD<String> words =  rdd.flatMap(l -> Arrays.asList(l.substring(l.lastIndexOf(",")+1 , l.length()-1)).iterator());
+            
+            Map<String, Long> wCount2= words.countByValue();
+            
+            writer.println(wCount2.toString());
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("ERROR: "+e.getMessage());
+        }
+    }
+     
+    
 	public static void main(String [] args)
 	{
+
+
 		// local environment (laptop/PC)
 		SparkConf conf = new SparkConf().setAppName("GRUPPE02").setMaster("local[*]");
 
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		String AWS_ACCESS_KEY_ID = "";
-		String AWS_SECRET_ACCESS_KEY = "";
+				
+		JavaRDD<String> rdd = sc.textFile("/data/kddcup.data_10_percent");
+				
+		//Check first line of the data
+		System.out.println(rdd.first());
+			
+		// Remove all non numeric features, to prepare rdd for Kmeans
+		JavaRDD<Vector> parsedData = parse(rdd);
+		
+		//List the clustering labels (last column) and their distinct counts
+		countLabels(rdd);
 
-		System.out.println(">>>>>>>>>>>>>>>>>> Hello from Spark! <<<<<<<<<<<<<<<<<<<<<<<<<");
-
-//  ...
-
-		// example accessing S3:
-
-/*
-    clusterMembers.saveAsTextFile("s3n://" + AWS_ACCESS_KEY_ID + ":" + AWS_SECRET_ACCESS_KEY + "@qltrail-lab-265-1488270472/result");
-*/
-		sc.stop();
-	}
-
+        
+	    sc.stop();
+    }
 }
